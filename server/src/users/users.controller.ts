554e1e71241e4d597c1a67d8c428af6f19711833
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-22 20:27:02 -0600

    added user dto

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserRequest } from './dto/create-user-request';
+import { UsersService } from './users.service';
+import { UserResponse } from './dto/user-response.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body() createUserRequest: CreateUserRequest,
+  ): Promise<UserResponse> {
+    return this.usersService.createUser(createUserRequest);
+  }
+}
