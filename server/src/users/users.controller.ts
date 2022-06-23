e3e1aca7be89c323ec2dc44a35a817b1192ae392
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-22 20:20:02 -0600

    modified controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,9 +3,4 @@ import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserRequest } from './dto/create-user-request';
 
 @Controller('users')
-export class UsersController {
-//   @Post()
-//   async createUser(@Body() createUserRequest: CreateUserRequest): Promise<any> {
-//     return this.userService.createUser(createUserRequest);
-//   }
-}
+export class UsersController {}
