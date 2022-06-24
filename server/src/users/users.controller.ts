a1818f73f943020fa5bea4e83357f9676a102b8b
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-23 18:59:58 -0600

    removed unusued vars

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserRequest } from './dto/create-user-request';
 import { UsersService } from './users.service';
 import { UserResponse } from './dto/user-response.dto';
